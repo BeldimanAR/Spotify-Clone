2161667fe7d96e8fa93543131fce98c07c7add15
Alexandru R. Beldiman <109196565+BeldimanAR@users.noreply.github.com>
2023-01-03 08:51:25 +0000

    Add files via upload

---
+++ src/components/ArtistCard.jsx
@@ -0,0 +1,5 @@
+const ArtistCard = () => {
+  <div>ArtistCafdrd</div>;
+};
+
+export default ArtistCard;
---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "b4baec63demsh82ce290be2b9635p11c4bejsnd35e8314e03d"
+      );
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({
+      query: () => "/charts/world",
+    }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
