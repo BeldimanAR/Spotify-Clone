27bc5106251179690d4c1726160d01f080626762
Alexandru R. Beldiman <roberto.beldiman10@gmail.com>
2023-01-03 15:47:54 +0200

    starting sidemenu

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,20 @@
-const Sidebar = () => (
-  <div>Sidebar</div>
-);
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { RiCloseLine } from "react-icons/ri";
+
+import { logo } from "../assets";
+// import { links } from "../assests/constants";
+
+const Sidebar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <>
+      <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
+        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+      </div>
+    </>
+  );
+};
 
 export default Sidebar;
